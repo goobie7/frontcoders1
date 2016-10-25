9546a271a412f4e2da4a4dd8368f609dc62d6a2e
Lukasz Dziedziola <dziedziola.lukasz@gmail.com>
2016-10-25 22:03:02 +0200

    Init new respository and files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-lol
+(function(){
+
+
+	$(document).ready(function() {
+		var $body = $('body'),
+			$mainNav = $('.main_navigation'),
+			$navOpenBtn = $('.btn_toggle_menu'),
+			$navCloseBtn = $('.main_navigation-close-btn');
+
+		$navOpenBtn.on('click', function() {
+			$body.addClass('nav-visible');
+		});
+
+		$navCloseBtn.on('click', function(){
+			$body.removeClass('nav-visible');
+		})
+	});
+
+
+})();
